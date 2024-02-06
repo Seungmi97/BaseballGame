805bf33ac09244dc96660d0bab943ac1ff2070f3
miiiingi <kimmingi722@gmail.com>
2024-02-06 15:19:10 +0900

    판단기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int comNumber = 356;
+        int trial = 1;
+        String strComNumber = String.valueOf(comNumber);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userNumber = sc.nextLine();
+            System.out.println(trial + "번째 시도 : " + userNumber);
+            if (userNumber.equals(strComNumber)) {
+                System.out.println(trial + "번만에 맞히셨습니다.");
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
+            int sCnt = 0;
+            int bCnt = 0;
+            for (int i = 0; i < 3; i++) {
+                if (strComNumber.charAt(i) == userNumber.charAt(i)) {
+                    sCnt++;
+                }
+                if (strComNumber.contains(String.valueOf(userNumber.charAt(i)))) {
+                    bCnt++;
+                }
+            }
+            String result = bCnt + "B" + sCnt + "S";
+            System.out.println(result);
+            trial++;
+        }
 
     }
 }
